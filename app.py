9fd03c85fcee7bb287f93da66bc3a36b12c83aea
Cptn Reef <sharif.staffordbz@gmail.com>
2021-01-12 18:20:39 -0500

    commiting

--- app.py
+++ app.py
@@ -1,44 +1,34 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template, url_for
 from pprint import PrettyPrinter
-from bs4 import BeautifulSoup
-
 import mongoengine as db
-import imdb
+import imdb 
 import json
 import os
 import requests
 import random
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def homepage():
-    return render_template('home.html')
-
-
 # database_name = "quickstart"
 # DB_URI = "mongodb+srv://MongoDBUser:wB2ULBQwOaFYLkKU@pythoncluster.zgpvn.mongodb.net/MongoDBUser?retryWrites=true&w=majority"
 # db.connect(DB_URI)
 
-# print(movies[0].keys())
+############################################################
+# SETUP
+############################################################
 
-# ------------ Search for a movie title
+app = Flask(__name__)
+
 moviesDB = imdb.IMDb()
 
 movies = moviesDB.search_movie('inception')
 
-# print('seatching for "Inception":')
-# for movie in movies:
-#     title = movie ['title']
-#     year = movie['year']
-#     print(f'{title} - {year}')
 
-# --------------------------------------
+print(movies)
 
+posters =  []
+for movie in movies:
+    posters.append(movie['full-size cover url'])
 
 
-# ------------ List movie info
 id = movies[0].getID()
 movie = moviesDB.get_movie(id)
 
@@ -47,6 +37,113 @@ year = movie['year']
 rating = movie['rating']
 directors = movie['directors']
 casting = movie['cast']
+poster = movie['cover url']
+full_poster = movie['full-size cover url']
+
+top = moviesDB.get_top250_movies()
+bottom = moviesDB.get_bottom100_movies()
+
+############################################################
+# MOVIE TITLE SEARCH
+############################################################
+
+@app.route('/')
+def homepage():
+    # type_in = request.form.get('movie_title')
+    
+    id = casting[0].getID()
+
+    person = moviesDB.get_person(id)
+    bio = moviesDB.get_person_biography(id)
+
+    name = person['name']
+    trivia = person['trivia']
+
+    context = {        
+        'movies': movies,
+        'title': title,
+        'year': year,
+        'rate': rating,
+        'cast': casting,
+        'poster': posters,
+        'big_poster': full_poster,
+        'top': top,
+        'bottom': bottom,
+        'name': name,
+        'trivia': trivia,
+    }
+
+    return render_template('home.html',**context)
+
+############################################################
+# MOVIE PAGE
+############################################################
+
+@app.route('/movie_page', methods=['GET', 'POST'])
+def moviepage():
+    
+    id = casting[0].getID()
+
+    person = moviesDB.get_person(id)
+    bio = moviesDB.get_person_biography(id)
+
+    name = person['name']
+    trivia = person['trivia']
+    movie_data = movies
+        
+    context = {
+        'movies': movie_data,
+        'title': title,
+        'cast': casting,
+        'poster': poster,
+        'rate': rating,
+        'big_poster': full_poster,
+        'top': top,
+        'bottom': bottom,
+        'name': name,
+        'trivia': trivia
+    }
+
+    return render_template('movie_page.html',**context)
+
+
+############################################################
+# SEARCH
+############################################################
+
+# @app.route('/search', methods=['GET', 'POST'])
+# def search():
+    
+#     if request.method == 'POST':
+        
+#         context = {
+#             'search': request.form.get('title_name'),
+#             'title': title,
+#             'cover': poster,
+#         }
+
+#         return render_template('search.html',**context)
+
+
+############################################################
+# DEBUG
+############################################################
+
+if __name__ == '__main__':
+    app.config['ENV'] = 'development'
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+############################################################
+# MOVIE DETAILS / REVIEW PAGE
+############################################################
 
 # print('Movie Info:')
 # print(f'{title} - {year}')
@@ -58,50 +155,16 @@ casting = movie['cast']
 # actors = ', '.join(map(str,casting))
 # print(f'actors: {actors}')
 
-# --------------------------------------
 
+############################################################
+# ACTOR INFO
+############################################################
 
-
-# ------------ List actor Info
-id = casting[0].getID()
-person = moviesDB.get_person(id)
-bio = moviesDB.get_person_biography(id)
-
-name = person['name']
-birthDate = person['birth date']
-height = person['height']
-trivia = person['trivia']
-titleRefs = bio['titlesRefs']
 
 # print(f'name: {name}')
-# print(f'birth date: {birthDate}')
-# print(f'height: {height}')
 # print(f'trivia: {trivia[0]}')
 
 # titleRefsStr = ', '.join(map(str, titleRefs))
 # print(f'bio title refs: {titleRefsStr}')
 
-# --------------------------------------
 
-
-
-# ------------ Top/Bottom 10 movies
-top = moviesDB.get_top250_movies()
-bottom = moviesDB.get_bottom100_movies()
-
-# print('Top 10 movies:')
-# for movie in top[0:9]:
-#     print(movie)
-
-# print('Bottom 10 movies:')
-# for movie in bottom[0:9]:
-#     print(movie)
-
-# --------------------------------------
-
-
-
-
-if __name__ == '__main__':
-    app.config['ENV'] = 'development'
-    app.run(debug=True)
